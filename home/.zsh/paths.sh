09e0f8d130d24a5fc186081e53e498f040b0d569
Nico <nico.niebergall@googlemail.com>
2020-01-02 16:32:04 +0100

    Added: rbenv, updated brewfile

--- home/.zsh/paths.sh
+++ home/.zsh/paths.sh
@@ -1,5 +1,10 @@
 # Homebrew
 export PATH="/usr/local/bin:$PATH"
 
-# Ruby
-export PATH="/usr/local/opt/ruby/bin:$PATH" 
+# Python 3.8
+export PATH="/usr/local/opt/python@3.8/bin:$PATH"
+
+# rbenv
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+eval "$(rbenv init -)"
+rbenv local 2.7.1
