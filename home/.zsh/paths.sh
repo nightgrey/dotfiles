a2542266c6f351f3e4330dab21d078c93ec192e8
Nico <nico.niebergall@googlemail.com>
2020-01-17 10:51:21 +0100

    Added: Path for ruby gems, alfred configuration update

--- home/.zsh/paths.sh
+++ home/.zsh/paths.sh
@@ -2,4 +2,5 @@
 export PATH="/usr/local/bin:$PATH"
 
 # Ruby
-export PATH="/usr/local/opt/ruby/bin:$PATH" 
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="/usr/local/lib/ruby/gems/2.6.0/bin:$PATH"
