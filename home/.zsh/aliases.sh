58c2274223c0e13420004527127d2e6c68cbd7ea
Nico <nico.niebergall@googlemail.com>
2020-02-23 15:49:36 +0100

    Changed: New formwizard alias

--- home/.zsh/aliases.sh
+++ home/.zsh/aliases.sh
@@ -4,12 +4,17 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+
 # HAUSGOLD: `env`
 alias start-verkaeuferportal="make -C ~/Dev/env start-analytic-api start-asset-api start-identity-api start-maklerportal-api start-verkaeuferportal-api start-property-api && make -C ~/Dev/env/projects/verkaeuferportal-frontend start"
 alias start-maklerportal="make -C ~/Dev/env start-identity-api start-asset-api start-maklerportal-api start-property-api && make -C ~/Dev/env/projects/maklerportal-frontend start"
 alias start-verkaeuferportal-maklerportal="make -C ~/Dev/env start-analytic-api start-identity-api start-asset-api start-maklerportal-api start-verkaeuferportal-api start-maklerportal-frontend start-property-api && make -C ~/Dev/env/projects/verkaeuferportal-frontend start"
+alias formizward="make -C ~/Dev/env start-formwizard-api && make -C ~/Dev/env/projects/formwizard-frontend start"
+
+# Unused / old
+#alias formwizard="make -C ~/Dev/env start-message-bus start-notification-distributor-processor start-notification-distributor-connector-kafka start-crm"
 #alias mpvp="make -C ~/Dev/env start-analytic-api start-identity-api start-asset-api start-maklerportal-api start-verkaeuferportal-api start-verkaeuferportal-frontend start-property-api && make -C ~/Dev/env/projects/maklerportal-frontend start"
-alias formwizard="make -C ~/Dev/env start-message-bus start-notification-distributor-processor start-notification-distributor-connector-kafka start-crm"
+
 
 # zsh
 alias reload="source ~/.zshrc"
