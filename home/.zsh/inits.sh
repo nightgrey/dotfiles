049c8d2dd40ff9ef6761af16850cd2075e674f31
Nico <nico.niebergall@googlemail.com>
2022-08-16 16:18:45 +0200

    Changed: Modified auto-nvm to only work in home/nico/Developers

--- home/.zsh/inits.sh
+++ home/.zsh/inits.sh
@@ -9,23 +9,32 @@ eval "$(rbenv init -)"
 source /usr/share/nvm/init-nvm.sh
 
 # Automagically use the NVM version defined in `.nvmrc`
+# Modified to only work if `pwd` matches `home/nico/Developer`.
 # https://github.com/nvm-sh/nvm#zsh
 autoload -U add-zsh-hook
 load-nvmrc() {
   local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
 
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+  if [[ $(pwd) = */home/nico/Developer* ]]; then
+    local nvmrc_path="$(nvm_find_nvmrc)"
 
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
+    if [ -n "$nvmrc_path" ]; then
+      local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+      if [ "$nvmrc_node_version" = "N/A" ]; then
+        nvm install
+      elif [ "$nvmrc_node_version" != "$node_version" ]; then
+        nvm use
+      fi
+    elif [ "$node_version" != "$(nvm version default)" ]; then
+      echo "Reverting to nvm default version"
+      nvm use default
     fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
+  else
+    if [ "$node_version" != "$(nvm version default)" ]; then
+      echo "Reverting to nvm default version"
+      nvm use default
+    fi
   fi
 }
 add-zsh-hook chpwd load-nvmrc
