e93d883d8f0e9f3f79e0fe5e17841137b599f1f5
Nico <nightgrey@users.noreply.github.com>
2024-03-09 08:46:13 +0100

    Changed: llama alias -> fn

--- home/.zsh/aliases/ai.zsh
+++ home/.zsh/aliases/ai.zsh
@@ -1,3 +1,7 @@
-alias llama="~/AI/llama.cpp/main"
+# Note: Needs to be a function for completions to work.
+llama() {
+    ~/AI/llama.cpp/main "$@"
+}
+
 alias comfy="~/AI/ComfyUI/venv/bin/python main.py --force-fp16"
 alias comfyui=comfy
