e65885f887b227f6003cf3e3a26d2f1bbc8027d1
Nico <nightgrey@users.noreply.github.com>
2024-03-09 09:04:29 +0100

    Changed: llama completions

--- home/.zsh/aliases/ai.zsh
+++ home/.zsh/aliases/ai.zsh
@@ -1,6 +1,11 @@
-# Note: Needs to be a function for completions to work.
+# Note: Needs to be a function for completions to apply, see completions folder.
+
 llama() {
     ~/AI/llama.cpp/main "$@"
+}
+
+llama-server() {
+    ~/AI/llama.cpp/server "$@"
 }
 
 alias comfy="~/AI/ComfyUI/venv/bin/python main.py --force-fp16"
