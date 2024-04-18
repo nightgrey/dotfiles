9b48576d706a8f7a026261d96c4bb07cee8efb44
Nico <nightgrey@users.noreply.github.com>
2024-04-18 17:12:00 +0200

    Added: Aliases for npx/bunx and the eslint config inspector

--- zsh/utils/node.zsh
+++ zsh/utils/node.zsh
@@ -1,3 +1,7 @@
+# Always use bun.
+alias _npx="npx"
+alias npx="bunx"
+
 # https://github.com/raineorshine/npm-check-updates
 # Check package updates with `npm-check-updates`
 alias ncu="bunx npm-check-updates"
@@ -5,3 +9,10 @@ alias ncu="bunx npm-check-updates"
 # https://github.com/dylang/npm-check
 # Check package updates with `npm-check`
 alias nc="bunx npm-check"
+
+
+# ESLint config inspector
+alias "inspect-eslint"="bunx @eslint/config-inspector"
+alias "eslint-inspect"="inspect-eslint"
+alias "debug-eslint"="inspect-eslint"
+alias "eslint-config"="inspect-eslint"
