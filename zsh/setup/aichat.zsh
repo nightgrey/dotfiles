125198050ffe6d8af034d9e7139abf8910d2b955
Nico <nightgrey@users.noreply.github.com>
2024-05-16 14:06:27 +0200

    Added: aichat terminal autocomplete

---
+++ zsh/setup/aichat.zsh
@@ -0,0 +1,16 @@
+# Integrates `aichat` into the shell.
+# Type something, then press `Ctrl + E` to get a response from `aichat`.
+# https://github.com/sigoden/aichat#shell-integration
+
+_aichat_zsh() {
+  if [[ -n "$BUFFER" ]]; then
+    local _old=$BUFFER
+    BUFFER+=" ..."
+    zle -I && zle redisplay
+    BUFFER=$(aichat -m claude:claude-3-sonnet-20240229 -e "$_old")
+    zle end-of-line
+  fi
+}
+zle -N _aichat_zsh
+
+bindkey '\ee' _aichat_zsh
