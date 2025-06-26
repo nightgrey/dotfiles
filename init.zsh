d5569b917eab6765f666a5060215b60f63390d46
Nico <nightgrey@users.noreply.github.com>
2025-06-26 22:27:58 +0200

    clean up

--- init.zsh
+++ init.zsh
@@ -1,17 +1,2 @@
 export DOTFILES=${0:A:h}
 export DOT=$DOTFILES
-
-
-# # Move to $DOTFILES/plugin or sth like that?
-# for script in $DOTFILES/zsh/{env,utils}/**/*.zsh; do
-#   source $script
-# done
-
-# export FPATH=$DOTFILES/zsh/completions:$FPATH
-# export PATH=$DOTFILES/zsh/bin:$PATH
-
-# for script in $DOTFILES/zsh/bin/*; do
-#     chmod +x $script
-# done
-
-# echo "hi"
