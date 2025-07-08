f3cb9a022af8216b03ba95de571878ba93ae24db
Nico <nightgrey@users.noreply.github.com>
2025-07-08 19:20:52 +0200

    Removed comment

--- misc/floating-ghostty.fish
+++ misc/floating-ghostty.fish
@@ -11,5 +11,4 @@ set H 550
 set X (string trim (math "$SCREEN_WIDTH / 2 - $W / 2"))
 set Y 0
 
-echo "Width: $W, Height: $H, X: $X, Y: $Y"
 env WAYLAND_DISPLAY=no tdrop -P 'wmctrl -i -r $wid -b add,above,skip_taskbar' -y $Y -x $X -w $W -h $H ghostty --config-file="/home/nico/.config/ghostty/floating.conf" --config-default-files=false
