4431a1effe41afd5bfa0126e4b73ea3494cc61f7
Nico <nightgrey@users.noreply.github.com>
2025-07-12 21:34:33 +0200

    load chezmoi completion

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,5 +1,6 @@
 oh-my-posh init fish -c ~/.config/oh-my-posh/config.json | source
 zoxide init fish | source
 mise activate fish | source
+chezmoi completion fish | source
 
 source ~/.config/fish/temp.fish
