f28d867059d5faaa3aa38cce074d5ea2b5ed8ed9
Nico <nightgrey@users.noreply.github.com>
2025-08-18 23:27:02 +0200

    Fixed: Formatting

--- home/dot_config/fish/conf.d/common.fish
+++ home/dot_config/fish/conf.d/common.fish
@@ -1,9 +1,9 @@
 # eza
 alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons' # long format
 alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
-alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+alias l.="eza -a | grep -e '^\.'" # show only dotfiles
 
 # editor
 alias ws=webstorm
@@ -19,7 +19,7 @@ alias cat=bat
 function history
     if test (count $argv) -gt 0
         builtin history $argv
-    else 
+    else
         builtin history --show-time='%F %T '
     end
 end
@@ -41,7 +41,6 @@ alias jctl="journalctl -p 3 -xb"
 # Recent installed packages
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
-
 function pairdrop -d "Open pairdrop"
-  open https://pairdrop.net/
+    open https://pairdrop.net/
 end
