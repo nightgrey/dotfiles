ce93ef8a93b1933f935e307ea8a8413d7519ed0c
Nico <nightgrey@users.noreply.github.com>
2025-08-18 23:27:47 +0200

    Added: ~fishsrc

--- home/dot_config/fish/conf.d/dirs.fish
+++ home/dot_config/fish/conf.d/dirs.fish
@@ -3,5 +3,7 @@
 abbr --add --position anywhere "~Dev" ~/Developer
 abbr --add --position anywhere "~dev" ~/Developer
 abbr --add --position anywhere "~dot" ~/.dot
+abbr --add --position anywhere "~fish" ~/.fish
+abbr --add --position anywhere "~fishsrc" /usr/share/fish
 abbr --add --position anywhere "~canvas" ~/Developer/canvas
-abbr --add --position anywhere "~canvas" ~/Developer/thing
+abbr --add --position anywhere "~thing" ~/Developer/thing
