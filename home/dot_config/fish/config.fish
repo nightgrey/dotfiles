2707c4e19db05df4cee2a1983b94d080693d46d5
Nico <nightgrey@users.noreply.github.com>
2025-09-30 23:42:58 +0200

    Added: Atuin, uv

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,7 +1,10 @@
 oh-my-posh init fish -c ~/.config/oh-my-posh/config.json | source
+
 zoxide init fish | source
 mise activate fish | source
 chezmoi completion fish | source
+atuin init fish | source
+atuin gen-completions --shell fish | source
+uv generate-shell-completion fish | source
 
 source ~/.config/fish/temp.fish
-
