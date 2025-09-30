9f5904f063a139cf28b192abe6a5c970ec40fe2f
Nico <nightgrey@users.noreply.github.com>
2025-09-30 23:43:46 +0200

    Added: LLM aliases

---
+++ home/dot_config/fish/functions/kimi.fish
@@ -0,0 +1,3 @@
+function kimi --description "Ask for help with Kimi K2"
+    ??? --model openrouter:moonshotai/kimi-k2-0905 $argv
+end
---
+++ home/dot_config/fish/functions/sonnet.fish
@@ -0,0 +1,3 @@
+function sonnet --description "Ask for help with Claude Sonnet"
+    ??? --model openrouter:anthropic/claude-sonnet-4.5 $argv
+end
